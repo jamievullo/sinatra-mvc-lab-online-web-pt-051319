b1309823deac98cdec961a5599c3681cb4184905
jamievullo <jvullo@protonmail.com>
2019-08-08 21:07:42 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-    
+    get '/' do
+        erb :user_input
+      end
 
+      
 end
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,4 +1,11 @@
 class PigLatinizer
 
+    def piglatinize(string)
+    #     vowel = [aeiou]
+    #     consonant.downcase = regex{/[aeiou]/}
+    #     if the string starts with a vowel add "way" to end of string
+    #     else add first letter if consanant and second letter if consanant and 
+    #         third letter if consonant to back of string add "ay"
+    end
 
 end
